0445adfef05147f3b85888092913f8c89a101151
임세현 <sh00710000@gmail.com>
2022-05-19 22:12:52 +0900

    📑 :: ErrorPage Sample

---
+++ 2022.05.19/임세현/ErrorSample/app/src/main/java/com/palette/errorsample/SampleExceptionHandler.kt
@@ -0,0 +1,77 @@
+package com.palette.errorsample
+
+import android.app.Activity
+import android.app.Application
+import android.content.Intent
+import android.content.Intent.EXTRA_INTENT
+import android.os.Bundle
+import android.os.Process
+import java.io.PrintWriter
+import java.io.StringWriter
+
+class SampleExceptionHandler(
+    application: Application,
+    private val crashlyticsExceptionHandler: Thread.UncaughtExceptionHandler
+) : Thread.UncaughtExceptionHandler {
+
+    private var lastActivity: Activity? = null
+    private var activityCount = 0
+
+    init {
+        application.registerActivityLifecycleCallbacks(
+            object : SimpleActivityLifecycleCallbacks() {
+
+                override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                    if (isSkipActivity(activity)) {
+                        return
+                    }
+                    lastActivity = activity
+                }
+
+                override fun onActivityStarted(activity: Activity) {
+                    if (isSkipActivity(activity)) {
+                        return
+                    }
+                    activityCount++
+                    lastActivity = activity
+                }
+
+                override fun onActivityStopped(activity: Activity) {
+                    if (isSkipActivity(activity)) {
+                        return
+                    }
+                    activityCount--
+                    if (activityCount < 0) {
+                        lastActivity = null
+                    }
+
+                }
+            })
+    }
+
+    private fun isSkipActivity(activity: Activity) = activity is ErrorActivity
+
+    override fun uncaughtException(thread: Thread, throwable: Throwable) {
+        lastActivity?.run {
+            val stringWriter = StringWriter()
+            throwable.printStackTrace(PrintWriter(stringWriter))
+
+            startErrorActivity(this, stringWriter.toString())
+        }
+        crashlyticsExceptionHandler.uncaughtException(thread, throwable)
+        Process.killProcess(Process.myPid())
+        System.exit(-1)
+    }
+
+    private fun startErrorActivity(activity: Activity, errorText: String) = activity.run {
+        val errorActivityIntent = Intent(this, ErrorActivity::class.java)
+            .apply {
+                putExtra(ErrorActivity.EXTRA_INTENT, intent)
+                putExtra(ErrorActivity.EXTRA_ERROR_TEXT, errorText)
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+        startActivity(errorActivityIntent)
+        finish()
+    }
+
+}
